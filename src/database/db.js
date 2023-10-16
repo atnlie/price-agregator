698694e2917c7fdfbb67af8f677bf7d0af8b606f
Atnlie <atnlie@gmail.com>
2023-10-16 19:35:54 +0700

    feat(env): add dot env and erd (#3)

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,15 @@
 const Pool = require('pg').Pool;
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.PG_PASSWORD)
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'price_agregator',
-    password: 'Admin2023!',
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
 module.exports = pool
